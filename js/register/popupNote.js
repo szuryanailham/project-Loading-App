e161970cf8f9b0cef8457dcbc278320d6922dfa7
ilham Suryana <szuryanailham090102@gmail.com>
2025-10-06 19:13:09 +0700

    memperbaiki pop up note register

--- js/register/popupNote.js
+++ js/register/popupNote.js
@@ -1,11 +1,11 @@
-function showNotes(notes) {
+window.showNotes = function (notes) {
     Swal.fire({
         title: "Catatan",
         text: notes,
         confirmButtonText: "Tutup",
-        confirmButtonColor: "#ef4444", // warna tombol merah
+        confirmButtonColor: "#ef4444",
         customClass: {
             popup: "rounded-xl shadow-lg",
         },
     });
-}
+};
